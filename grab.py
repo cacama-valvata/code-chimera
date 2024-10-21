e60d7f0cdd559b2a6738d9e3bba886f8fff2f7c5
Casey Colley <code@caseycolley.com>
2024-10-21 12:49:31 -0700

    skeleton with unrelated filter code

---
+++ grab.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+# https://docs.github.com/en/search-github/searching-on-github/searching-code
+def grab_github ():
+    github_pat = open('github.pat', 'r').read().strip()
+
+    u = "https://api.github.com/search/code"
+    f = "filename:sshd_config"
+
+    url = u + "?q=" + f + "&per_page=100"
+
+    g_resp = requests.get(url,
+                          headers= {'Authorization': 'Bearer ' + github_pat,
+                                    'Accept': 'application/vnd.github+json'} )
+    print(g_resp)
+    repos = json.loads(g_resp.text)
+    print(repos["total_count"])
+    #print(json.dumps(repos, indent=4))
+
+grab_github()
