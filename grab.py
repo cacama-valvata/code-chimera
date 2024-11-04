7efe59f3e3c5a87456e13281b54eb4d562d25301
Casey Colley <code@caseycolley.com>
2024-11-04 10:47:03 -0800

    using api fuzzy search

--- grab.py
+++ grab.py
@@ -8,17 +8,19 @@ def grab_github ():
 
     u = "https://api.github.com/search/code"
     f = "filename:/sshd_config/"
+    f = "stackoverflow.com in:file language:python"
 
-    print(urllib.parse.quote(f, safe=''))
+    f_q = urllib.parse.quote(f, safe='')
+    print(f_q)
 
-    url = u + "?q=" + f + "&per_page=100"
+    url = u + "?q=" + f_q + "&per_page=100"
 
     g_resp = requests.get(url,
                           headers= {'Authorization': 'Bearer ' + github_pat,
-                                    'Accept': 'application/vnd.github+json'} )
+                                    'Accept': 'application/vnd.github.text-match+json'} )
     print(g_resp)
     repos = json.loads(g_resp.text)
-    print(repos["total_count"])
-    #print(json.dumps(repos, indent=4))
+    #print(repos["total_count"])
+    print(json.dumps(repos, indent=4))
 
 grab_github()
