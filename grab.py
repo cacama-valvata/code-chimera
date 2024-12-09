9700dd49c2519f8666bfe86bc2efa8360ab75657
Casey Colley <code@caseycolley.com>
2024-12-08 20:14:05 -0800

    get the real samples

--- grab.py
+++ grab.py
@@ -3,6 +3,7 @@ import json
 import urllib
 import subprocess
 from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import Process, Queue
 
 # https://docs.github.com/en/search-github/searching-on-github/searching-code
 def grab_github ():
@@ -56,9 +57,95 @@ def grab_github ():
     #print(json.dumps(repos, indent=4))
 
 
+def redo_links():
+    def repo_process(repo_url):
+        if repo_url in last:
+            #print("duplicate: " + repo_url)
+            return
+
+        cmd = ["bash", "process.sh", repo_url]
+        ret_cmd = subprocess.run(cmd, capture_output=True)
+        if ret_cmd.returncode != 0:
+            print(f"1. Errored at {repo_url} with code {ret_cmd.returncode}\nOutput:")
+            print(f"{ret_cmd.stderr.decode("utf-8")}")
+            teardown_repo(repo_url)
+            return
+        
+        output = ret_cmd.stdout.decode("utf-8").strip().splitlines()
+        for i in range(len(output)):
+            output[i] = output[i].strip()
+        output = [x for x in output if x] # remove empty strings
+        if len(output)//3 != len(output)/3:
+            print(f"2. Errored at {repo_url}\n{len(output)//3}  {len(output)/3}\nOutput:")
+            print(f"{ret_cmd.stdout.decode("utf-8")}")
+            teardown_repo(repo_url)
+            return
+        num_urls = len(output)//3
+
+        to_return = []
+        for i in range(num_urls):
+            new_url = output[i + 0*num_urls]
+            new_date = output[i + 1*num_urls]
+            new_lineno = output[i + 2*num_urls]
+            to_return.append( (repo_url, new_url, new_date, new_lineno) )
+
+        print(f"Success: {repo_url.split('/')[4]}")
+        writing_q.put(to_return)
+        teardown_repo(repo_url)
+        return
+
+    def teardown_repo(repo_url):
+        # ['https:', '', 'github.com', 'kristovatlas', 'multi-sig-check-demo', 'blob', '78231bb8cff94323d0fff827f745db1905146028', 'examples', 'osx-config-check-master', 'app.py']
+        repo_dir = repo_url.split('/')[4]
+
+        td_cmd = ["rm", "-rf", "/tmp/workdir/" + repo_dir]
+        ret_td = subprocess.run(td_cmd, capture_output=True)
+        return
+
+    def record_results (queue):
+        while True:
+            if not queue.empty():
+                result = queue.get()
+                if result == "DONE":
+                    break
+                for old_url, url, date, line_number in result:
+                    with open("github.cache", "a") as f:
+                        f.write(old_url + "\n")
+                    with open("github.list.output.new", "a") as f:
+                        f.write(url + "\n")
+                    with open("github.dates.output.new", "a") as f:
+                        f.write(date + "\n")
+                    with open("github.lines.output.new", "a") as f:
+                        f.write(line_number + "\n")
+        return
+
+
+    # for resuming
+    try:
+        last_repos = open('github.cache', 'r').read().splitlines()
+        last = set(last_repos)
+    except FileNotFoundError:
+        last = set()
+
+    # print(len(last))
+    # exit()
+
+    writing_q = Queue()
+    record_results_p = Process(target=record_results, args=(writing_q,))
+    record_results_p.start()
+
+    repos = open("github.list.output", "r").read().splitlines()
+    pool = ThreadPool(1)
+    output = pool.map(repo_process, repos)
+
+    writing_q.put("DONE")
+    record_results_p.join()
+
+    print("done :3")
+
 def dl_gh():
     def repo_process(repo_url):
-        resdir = "results"
+        resdir = "samples"
         resfn = repo_url[18:].replace("/", "_")
         
         response = requests.get(repo_url.replace("blob", "raw"))
@@ -71,10 +158,11 @@ def dl_gh():
         return
 
     repos = open("github.list.output", "r").read().splitlines()
-    pool = ThreadPool(50)
+    pool = ThreadPool(1)
     pool.map(repo_process, repos)
     print("done :3")
 
 
 #grab_github()
-dl_gh()
+#dl_gh()
+redo_links()
--- process.sh
+++ process.sh
@@ -1,9 +1,41 @@
-# Weeeeee
+#!/bin/bash
 set -euo pipefail
 
-cd workdir
-git clone $1
-dir=$(basename $1)
-cd $dir
+# example $1 = https://github.com/kristovatlas/multi-sig-check-demo/blob/78231bb8cff94323d0fff827f745db1905146028/examples/osx-config-check-master/app.py
 
-git --no-pager log -p --follow $2
+clone_url=${1%/blob/*}
+clone_dir=$(basename $clone_url)
+file_path=${1#*blob/*/}
+file_path=$(printf '%b' "${file_path//%/\\x}")
+file_path=$(printf '%s' "${file_path// /\\ }") # take care of spaces in file paths
+file_path=$(printf '%s' "${file_path//[/\\[}") # only one sample needed these two statements
+file_path=$(printf '%s' "${file_path//]/\\]}")
+old_commit=${1#*blob/}
+old_commit=${old_commit%%/*}
+
+cd /tmp/workdir
+git clone $clone_url 2>/dev/null
+cd "./$clone_dir"
+
+gitlog_output=$(git --no-pager log -p --follow -- "$file_path")
+#commit_lines=$(echo "$gitlog_output" | grep -n "commit " - | grep ":commit" - | cut -d ':' -f1)
+so_lines=$(echo "$gitlog_output" | grep -n "stackoverflow.com" | cut -d ":" -f1)
+
+new_urls=""
+new_dates=""
+while read -r line; do
+    line_number=$(echo "$gitlog_output" | sed -n "1, $line p" - | grep -n "commit " - | grep ":commit" - | cut -d ':' -f1 | tail -n 1)
+    date_line_number=$(("$line_number+2"))
+    commit_line=$(echo "$gitlog_output" | sed -n "$line_number p")
+    commit_hash=${commit_line#commit\ }
+    date_line=$(echo "$gitlog_output" | sed -n "$date_line_number p")
+    date=${date_line#Date:\ \ \ }
+    # There's always at least line 1 so I'm not going to check for empty output before filtering to commit_hash
+    new_url="${clone_url}/blob/${commit_hash}/${file_path}"
+    new_urls+="${new_url}\n"
+    new_dates+="${date}\n"
+done <<< "$so_lines"
+
+echo -e "$new_urls"
+echo -e "$new_dates"
+echo -e "$so_lines"
